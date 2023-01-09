987db22b65b1f9f94b0f548a5aee5936fc01e9da
Rahul <rsc120@nist.edu>
2023-01-09 20:32:55 +0530

    Code Updated

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int pageIdx = 0;
-  TextEditingController textController=TextEditingController();
+  TextEditingController textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,84 +53,128 @@ class _HomeScreenState extends State<HomeScreen> {
               label: 'Account'),
         ],
       ),
-      body: Column(children: [
-        SafeArea(
-          child: Container(
-            height: MediaQuery.of(context).size.height / 3,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: const Color(0xFF0E3C6E),
-            ),
-            child: Column(children: [
-              Row(
-                children: [
-                  Container(
-                      margin: EdgeInsets.fromLTRB(20, 50, 170, 20),
-                      child: Text(
-                        "Find your own way",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.white),
-                      )),
-                  Container(
-                    child: Stack(children: <Widget>[
-                      Icon(
-                        Icons.notifications,
-                        color: Colors.white,
-                      ),
-                      Positioned(
-                        top: 0.0,
-                        right: 0.0,
-                        child: Icon(Icons.brightness_1,
-                            size: 8.0, color: Colors.redAccent),
-                      )
-                    ]),
-                  )
-                ],
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SafeArea(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 3,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color(0xFF0E3C6E),
               ),
-              Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 170, 0),
-                  child: Text(
-                    "Search in 600 colleges around!",
-                    style: TextStyle(fontSize: 15, color: Colors.white),
-                  )),
-              Container(
-                child:Row(
+              child: Column(children: [
+                Row(
                   children: [
                     Container(
-                      margin: EdgeInsets.fromLTRB(10, 20, 30, 10),
-                      width:MediaQuery.of(context).size.width-100,
-                      decoration:BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(70),
-                      ),
-                      child: TextField(
-                        controller: textController,
-                        decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search for colleges, schools.....",
-                        prefixIcon: Icon(Icons.search),
-
-                      ),
-                      )),
+                        margin: EdgeInsets.fromLTRB(20, 50, 170, 20),
+                        child: Text(
+                          "Find your own way",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.white),
+                        )),
                     Container(
-                      height:40,
-                      width:40,
-                      decoration: BoxDecoration(
-                        color:Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child:Icon(Icons.mic,color: Colors.black,)
+                      child: Stack(children: <Widget>[
+                        Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        ),
+                        Positioned(
+                          top: 0.0,
+                          right: 0.0,
+                          child: Icon(Icons.brightness_1,
+                              size: 8.0, color: Colors.redAccent),
+                        )
+                      ]),
                     )
                   ],
-                )
-              ),
-            ]),
+                ),
+                Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 170, 0),
+                    child: Text(
+                      "Search in 600 colleges around!",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    )),
+                Container(
+                    child: Row(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.fromLTRB(10, 20, 30, 10),
+                        width: MediaQuery.of(context).size.width - 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(70),
+                        ),
+                        child: TextField(
+                          controller: textController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search for colleges, schools.....",
+                            prefixIcon: Icon(Icons.search),
+                          ),
+                        )),
+                    Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.mic,
+                          color: Colors.black,
+                        ))
+                  ],
+                )),
+              ]),
+            ),
           ),
-        )
-      ]),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width-30,
+            height: MediaQuery.of(context).size.height/5,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://images.unsplash.com/photo-1579202673506-ca3ce28943ef"),
+                    fit: BoxFit.cover)),
+          ),
+            SizedBox(
+            height: 40,
+          ),
+           Container(
+            width: MediaQuery.of(context).size.width-30,
+            height: MediaQuery.of(context).size.height/5,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://images.unsplash.com/photo-1579202673506-ca3ce28943ef"),
+                    fit: BoxFit.cover)),
+          ),
+            SizedBox(
+            height: 40,
+          ),
+           Container(
+            width: MediaQuery.of(context).size.width-30,
+            height: MediaQuery.of(context).size.height/5,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://images.unsplash.com/photo-1579202673506-ca3ce28943ef"),
+                    fit: BoxFit.cover)),
+          ),
+            SizedBox(
+            height: 40,
+          ),
+        ]),
+      ),
     );
   }
 }
